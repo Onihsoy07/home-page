957aabac17bd8d9239d8b54c00223a1a3e69fab5
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-11 23:46:52 +0900

    구현 : Board Entity 생성

---
+++ home-page/src/main/java/com/example/homepage/domain/entity/Board.java
@@ -0,0 +1,26 @@
+package com.example.homepage.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(schema = "home_page")
+public class Board extends Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false, unique = false)
+    private String title;
+
+    @Lob
+    @Column(nullable = false, unique = false)
+    private String Contents;
+
+}
