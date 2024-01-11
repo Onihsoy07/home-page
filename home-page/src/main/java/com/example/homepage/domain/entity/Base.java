e620c98cb359ac13763bbbebf0eca471fdd799a9
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-11 23:40:58 +0900

    구현 : Entity 생성, 변경 추가 기본 베이스

---
+++ home-page/src/main/java/com/example/homepage/domain/entity/Base.java
@@ -0,0 +1,26 @@
+package com.example.homepage.domain.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Base {
+
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+
+}
