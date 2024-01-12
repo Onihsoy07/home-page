d5beed853ec22fd900416db7e7c4d8c35fc05b02
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-12 23:59:16 +0900

    구현 : BoardService(CRUD 기본)

---
+++ home-page/src/main/java/com/example/homepage/service/BoardService.java
@@ -0,0 +1,47 @@
+package com.example.homepage.service;
+
+import com.example.homepage.domain.dto.BoardDto;
+import com.example.homepage.domain.dto.BoardUpdateDto;
+import com.example.homepage.domain.entity.Board;
+import com.example.homepage.domain.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional(readOnly = true)
+    public List<BoardDto> getList() {
+        return BoardDto.convertToList(boardRepository.findAll());
+    }
+
+    @Transactional(readOnly = true)
+    public BoardDto findByBoard(Long boardId) {
+        return new BoardDto(findByBoardEntity(boardId));
+    }
+
+    @Transactional(readOnly = true)
+    public Board findByBoardEntity(Long boardId) {
+        return boardRepository.findById(boardId).orElseThrow(() -> {
+            throw new IllegalArgumentException(String.format("Board ID %d로 찾을 수 없습니다.", boardId));
+        });
+    }
+
+    public void updateBoard(Long boardId, BoardUpdateDto boardUpdateDto) {
+        findByBoardEntity(boardId).updateBoard(boardUpdateDto);
+    }
+
+    public void deleteBoard(Long boardId) {
+        boardRepository.deleteById(boardId);
+    }
+
+}
