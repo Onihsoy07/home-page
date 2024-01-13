14109a7c1b501f906e372f39d16b3fa2af4df75f
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-13 22:41:29 +0900

    구현 : BoardSaveDto

---
+++ home-page/src/main/java/com/example/homepage/domain/dto/BoardSaveDto.java
@@ -0,0 +1,17 @@
+package com.example.homepage.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class BoardSaveDto {
+
+    @NotBlank(message = "제목을 입력해주세요")
+    private String title;
+
+    @NotBlank(message = "내용을 입력해주세요")
+    private String Contents;
+
+}
