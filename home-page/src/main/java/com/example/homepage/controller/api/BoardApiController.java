32fd9040585e04b18cf2a892cf21fe1fa7b90ee1
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-14 23:53:02 +0900

    구현 : BoardApiController 각 swagger 이름 추가

--- home-page/src/main/java/com/example/homepage/controller/api/BoardApiController.java
+++ home-page/src/main/java/com/example/homepage/controller/api/BoardApiController.java
@@ -4,6 +4,7 @@ import com.example.homepage.domain.dto.BoardDto;
 import com.example.homepage.domain.dto.BoardSaveDto;
 import com.example.homepage.domain.dto.BoardUpdateDto;
 import com.example.homepage.service.BoardService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +25,7 @@ public class BoardApiController {
     private final BoardService boardService;
 
     @PostMapping
+    @Tag(name = "게시물 저장", description = "게시물 저장 가능")
     public ResponseEntity<Void> saveBoard(@Valid @RequestBody final BoardSaveDto boardSaveDto,
                                           BindingResult bindingResult) {
         boardService.saveBoard(boardSaveDto);
@@ -31,18 +33,21 @@ public class BoardApiController {
     }
 
     @GetMapping
+    @Tag(name = "모든 게시물 조회", description = "모든 게시물 조회 가능")
     public ResponseEntity<List<BoardDto>> getAllBoard() {
         List<BoardDto> boardDtoList = boardService.getList();
         return ResponseEntity.status(HttpStatus.OK).body(boardDtoList);
     }
 
     @GetMapping("/{boardId}")
+    @Tag(name = "게시물 단일 조회", description = "게시물 ID로 조회 가능")
     public ResponseEntity<BoardDto> getBoard(@PathVariable("boardId") final Long boardId) {
         BoardDto boardDto = boardService.getBoard(boardId);
         return ResponseEntity.status(HttpStatus.OK).body(boardDto);
     }
 
     @PutMapping("/{boardId}")
+    @Tag(name = "게시물 변경", description = "게시물 제목, 내용 변경 가능")
     public ResponseEntity<Void> updateBoard(@PathVariable("boardId") final Long boardId,
                                             @Valid @RequestBody final BoardUpdateDto boardUpdateDto,
                                             BindingResult bindingResult) {
@@ -51,6 +56,7 @@ public class BoardApiController {
     }
 
     @DeleteMapping("/{boardId}")
+    @Tag(name = "게시물 삭제", description = "게시물 삭제 가능")
     public ResponseEntity<Void> deleteBoard(@PathVariable("boardId") final Long boardId) {
         boardService.deleteBoard(boardId);
         return ResponseEntity.status(HttpStatus.OK).build();
