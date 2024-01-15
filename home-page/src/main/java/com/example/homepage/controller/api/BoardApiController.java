d4a8f2cde9ae91ccc17fc903a855363b92047dc3
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-15 23:39:23 +0900

    구현 : swagger에서 파라매터 이름 및 예시 지정
    일부 구현

--- home-page/src/main/java/com/example/homepage/controller/api/BoardApiController.java
+++ home-page/src/main/java/com/example/homepage/controller/api/BoardApiController.java
@@ -4,6 +4,14 @@ import com.example.homepage.domain.dto.BoardDto;
 import com.example.homepage.domain.dto.BoardSaveDto;
 import com.example.homepage.domain.dto.BoardUpdateDto;
 import com.example.homepage.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +34,13 @@ public class BoardApiController {
 
     @PostMapping
     @Tag(name = "게시물 저장", description = "게시물 저장 가능")
+    @Operation(method = "POST", description = "게시물 저장", parameters = {
+            @Parameter(in = ParameterIn.DEFAULT, name = "data", required = true, schema = @Schema(implementation = BoardSaveDto.class))
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "500", description = "SERVER ERROR")
+    })
     public ResponseEntity<Void> saveBoard(@Valid @RequestBody final BoardSaveDto boardSaveDto,
                                           BindingResult bindingResult) {
         boardService.saveBoard(boardSaveDto);
@@ -41,6 +56,11 @@ public class BoardApiController {
 
     @GetMapping("/{boardId}")
     @Tag(name = "게시물 단일 조회", description = "게시물 ID로 조회 가능")
+    @Parameter(name = "boardId", example = "1", description = "게시물 아이디")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = BoardDto.class))),
+            @ApiResponse(responseCode = "500", description = "SERVER ERROR")
+    })
     public ResponseEntity<BoardDto> getBoard(@PathVariable("boardId") final Long boardId) {
         BoardDto boardDto = boardService.getBoard(boardId);
         return ResponseEntity.status(HttpStatus.OK).body(boardDto);
--- home-page/src/main/java/com/example/homepage/domain/dto/BoardSaveDto.java
+++ home-page/src/main/java/com/example/homepage/domain/dto/BoardSaveDto.java
@@ -1,5 +1,6 @@
 package com.example.homepage.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,11 @@ import lombok.NoArgsConstructor;
 public class BoardSaveDto {
 
     @NotBlank(message = "제목을 입력해주세요")
+    @Schema(description = "게시물 제목", nullable = false, example = "안녕하세요")
     private String title;
 
     @NotBlank(message = "내용을 입력해주세요")
-    private String Contents;
+    @Schema(description = "게시물 내용", nullable = false, example = "안녕하세요 내용 ~~")
+    private String contents;
 
 }
