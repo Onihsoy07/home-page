203284eeac626912a30195dcea326f1b5622a4f3
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-19 00:01:28 +0900

    프론트 구현 : 간단한 boardList 확인

--- front/home-page/src/App.vue
+++ front/home-page/src/App.vue
@@ -1,8 +1,34 @@
 <template>
-  
+  <div>
+    <div v-for="board in data.boardList" :key="board.id" style="border: solid black 1px; border-collapse: collapse;">
+      <div>{{ board.title }}</div>
+      <div>{{ board.contents }}</div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { reactive, onMounted } from 'vue';
+
+const data = reactive({
+  boardList: []
+});
+
+function getBoardList() {
+  fetch('http://localhost:9000/boards')
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+  .then(response => {
+    console.log(response);
+    data.boardList = response.data;
+  });
+}
+
+onMounted(() => {
+  getBoardList();
+});
 
 </script>
 
